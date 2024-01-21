76e2abd8911eb6f71881d803b7cd64168d99fb0d
jawadre <133800106+jawadre@users.noreply.github.com>
2024-01-21 16:29:34 +0000

    Update videos.js
    Possible speed improvement

--- videos.js
+++ videos.js
@@ -1,49 +1,39 @@
-
-// Get the divs containing each portfolio item
 const portfolioItems = document.querySelectorAll('.js-video-button');
 const modal = document.querySelector('.modal-video');
 
-// Add an Event Listener for a click to each item
-
 portfolioItems.forEach(item => {
-
     item.addEventListener('click', function(event) {
-        
-        // Stop the click event from propagating to the document object
         event.stopPropagation();
 
-        // Get video url from the data-video-url attribute
         const videoUrl = item.getAttribute('data-video-url');
 
         modal.innerHTML = `
         <div class="modal">
-            <iframe 
-                src="${videoUrl}" 
-                title="YouTube video player" 
-                frameborder="0" 
-                allow=
-                "accelerometer; 
-                autoplay; 
-                clipboard-write; 
-                encrypted-media; 
-                gyroscope; 
-                picture-in-picture; 
-                web-share" 
-                allowfullscreen
-            ></iframe>
+            <div class="video-container"></div>
         </div>
         `;
 
-        // Show the modal
+        const videoContainer = modal.querySelector('.video-container');
+
+        // Create iframe element dynamically
+        const iframe = document.createElement('iframe');
+        iframe.src = videoUrl;
+        iframe.title = "YouTube video player";
+        iframe.frameBorder = 0;
+        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+        iframe.allowFullscreen = true;
+
+        // Append the iframe to the video container
+        videoContainer.appendChild(iframe);
+
         modal.style.display = "flex";
 
-        //Smooth Opacity Transition
+        // Smooth Opacity Transition
         setTimeout(() => {
             modal.style.opacity = "1";
         }, 50);
     });
 });
-
 
 document.body.addEventListener('click', () => {
     if (modal.innerHTML !== '') {
@@ -54,8 +44,5 @@ document.body.addEventListener('click', () => {
         setTimeout(() => {
             modal.style.display = "none";
         }, 450);
-    } else {
-        null;
     }
-    
 });
